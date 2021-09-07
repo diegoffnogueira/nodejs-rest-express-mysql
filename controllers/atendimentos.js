736b5063e28dae7fcb9cf3eee7a29ef3f36c30e5
Diego Nogueira <diego.nogueira@m4u.com.br>
2021-09-07 00:58:31 -0300

    Creating the controller

---
+++ controllers/atendimentos.js
@@ -0,0 +1,13 @@
+const Atendimento = require('../models/atendimentos')
+
+module.exports = app => {
+    app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos com GET'))
+
+    app.post('/atendimentos', (req, res) => {
+        const atendimento = req.body
+        Atendimento.create(atendimento, res)
+        // res.send('Atendimento cadastrado!')
+    })
+}
+
+
