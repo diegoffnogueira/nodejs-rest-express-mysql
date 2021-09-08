879a364d24d8e9783fc87b56559e4c2a366dee71
Diego Nogueira <diego.nogueira@m4u.com.br>
2021-09-08 01:30:25 -0300

    Creating the complete CRUD for the service

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,12 +1,30 @@
 const Atendimento = require('../models/atendimentos')
 
 module.exports = app => {
-    app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos com GET'))
+    app.get('/atendimentos', (req, res) => {
+        Atendimento.list(res)
+    })
+
+    app.get('/atendimentos/:id', (req, res) => {
+        const id = parseInt(req.params.id)
+        Atendimento.listById(res, id)
+    })
 
     app.post('/atendimentos', (req, res) => {
         const atendimento = req.body
         Atendimento.create(atendimento, res)
         // res.send('Atendimento cadastrado!')
+    })
+
+    app.patch('/atendimentos/:id', (req, res) => {
+        const id = parseInt(req.params.id)
+        const valores = req.body
+        Atendimento.update(id, res, valores)
+    })
+
+    app.delete('/atendimentos/:id', (req, res) => {
+        const id = parseInt(req.params.id)
+        Atendimento.delete(id, res)
     })
 }
 
